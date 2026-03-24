f4b9067b1db9058636841c7c9982ae325b36c062
VladCR777 <235779393+pandacloud3@users.noreply.github.com>
2026-03-24 17:53:59 +0200

    Create Patient.py

---
+++ dentist/app/db/models/Patient.py
@@ -0,0 +1,9 @@
+from app import db
+
+class Patient(db.Model):
+    __tablename__ = 'client'
+
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(255))
+    phone = db.Column(db.String(255))
+    email = db.Column(db.String(255))
