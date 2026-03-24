c3889dbfa38d53f588c9629e2e942f1a221e154e
VladCR777 <235779393+pandacloud3@users.noreply.github.com>
2026-03-24 18:02:58 +0200

    add dal

---
+++ dentist/app/dal/app_dal/__init__.py
@@ -0,0 +1,3 @@
+from app.dal.app_dal.app_dal import AppDAL
+
+app_dal = AppDAL()
---
+++ dentist/app/dal/app_dal/app_dal.py
@@ -0,0 +1,11 @@
+from app.dal.app_dal.I_app_dal import IAppDAL
+
+class AppDAL(IAppDAL):
+    def read_csv(self):
+        pass
+
+    def create_db(self):
+        pass
+
+    def write_db(self):
+        pass
